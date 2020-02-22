b97162abd68968f51daec91fadeef2886cef738e
Luca Bognolo <git@bogny.eu>
2020-02-22 02:00:10 +0100

    Fixed User model
    Signed-off-by: Luca Bognolo <git@bogny.eu>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,8 +1,6 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -17,6 +15,7 @@ use Illuminate\Support\Str;
 |
 */
 
+/* @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
